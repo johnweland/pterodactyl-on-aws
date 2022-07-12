9ede78013e14ddcc841b9c97edda4a8f49fa0907
John Weland <john.weland@gmail.com>
2022-07-12 11:44:15 -0500

    begin to setup panel ec2

--- cdk/lib/pterodactyl-panel-stack.ts
+++ cdk/lib/pterodactyl-panel-stack.ts
@@ -8,19 +8,44 @@ export class PanelStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const vpc = ec2.Vpc.fromLookup(this, 'vpc', {vpcId: 'vpc-123456'});
-
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      maxAzs: 2,
+      cidr: '10.0.0.0/16',
+      subnetConfiguration: [
+        {
+          cidrMask: 16,
+          name: 'protected',
+          subnetType: ec2.SubnetType.PUBLIC,
+        }],
+    });
     const launchConfiguration = new autoscaling.CfnLaunchConfiguration(this, 'LaunchConfiguration', {
-      imageId: '',
-      instanceType: ''
+      imageId: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2022,
+        edition: ec2.AmazonLinuxEdition.STANDARD,
+        storage: ec2.AmazonLinuxStorage.EBS,
+      }).getImage(this).imageId,
+      instanceType: 't4.micro',
+      associatePublicIpAddress: true,
+      blockDeviceMappings: [
+        {
+          deviceName: '/dev/xvda',
+          ebs: {
+            volumeSize: 50,
+            volumeType: 'gp3',
+            deleteOnTermination: true,
+            encrypted: true,
+          },
+        }
+      ],
+      ebsOptimized: true
     });
 
-    const scalingPolicy = new autoscaling.CfnScalingPolicy(this, 'blah', {
+    const scalingPolicy = new autoscaling.CfnScalingPolicy(this, `${this.stackName}-asg-policy`, {
         autoScalingGroupName: 'pterodactyl-panel-asg',
     })
 
-    const autoscaler = new autoscaling.AutoScalingGroup(this, 'asg', {
-      vpc
+    const autoscaler = new autoscaling.AutoScalingGroup(this, `${this.stackName}-asg`, {
+      vpc,
     });
   }
 }
